de5e23f0c0a2da71646d17d56328cb0cf73808df
Amaury Simondet <amaury.simondet@hotmail.com>
2025-04-14 12:17:13 +0200

    begin

--- App.js
+++ App.js
@@ -1,52 +1,10 @@
-import { StyleSheet, View, Text, StatusBar, TextInput, Button, ImageBackground } from 'react-native';
-import StartScreen from './screens/StartScreen';
-import GameScreen from './screens/GameScreen';
-import ResultScreen from './screens/ResultScreen';
-import { useState } from 'react';
-import { LinearGradient } from 'expo-linear-gradient';
-import { useFonts, Poppins_400Regular } from "@expo-google-fonts/poppins";
-import AppLoading from 'expo-app-loading';
-import Colors from './constants/colors';
+import { StyleSheet, View, Text } from 'react-native';
 
 export default function App() {
-  const [number, setNumber] = useState(0);
-  const [step, setStep] = useState(0);
-  const [count, setCount] = useState(0);
-
-  const [fontsLoaded] = useFonts({ Poppins_400Regular });
-
-  if (!fontsLoaded) {
-    return <AppLoading />;
-  }
-
-  const handleValidate = (num) => {
-    setNumber(num);
-    setStep(1);
-  }
-
-  const handleGoBack = () => {
-    setStep(0);
-  }
-
-  const handleGuess = (count) => {
-    setStep(2);
-    setCount(count);
-  }
-
-  const handlePlayAgain = () => {
-    setStep(0);
-    setNumber(0);
-    setCount(0);
-  }
-
   return (
-    <LinearGradient colors={[Colors.primary300, Colors.primary500]} style={styles.container}>
-      <ImageBackground source={require('./assets/images/background.png')} resizeMode='cover' style={styles.container} imageStyle={{ opacity: 0.2 }}>
-        {step === 0 && <StartScreen onValidate={handleValidate} number={number} />}
-        {step === 1 && <GameScreen number={number} onGoBack={handleGoBack} onGuess={handleGuess} />}
-        {step === 2 && <ResultScreen number={number} count={count} onPlayAgain={handlePlayAgain} />}
-      </ImageBackground>
-    </LinearGradient>
+    <View style={styles.container}>
+      <Text>Hello World</Text>
+    </View>
   );
 }
 
--- constants/colors.jsx
+++
@@ -1,6 +0,0 @@
-const Colors = {
-    primary300: '#F0B5B5FF',
-    primary500: '#9b0000',
-}
-
-export default Colors;
